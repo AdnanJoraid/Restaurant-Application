559f1f87a4936596038dba1d4ad360d9c3eeafdc
Adnan Joraid <joraid@shernet.sheridancollege.ca>
2020-04-02 01:34:59 -0400

    Added classes and pages folders

--- Group2_CS_FinalProject/App.xaml.cs
+++ Group2_CS_FinalProject/App.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Group2_CS_FinalProject.Pages;
 
 namespace Group2_CS_FinalProject
 {
