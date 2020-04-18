5cce80bacd0b0490b15fe5923c7ff721f63e35fd
Adam <czubernatadam@gmail.com>
2020-04-18 13:24:57 -0400

    change List of Drinks

--- Group2_CS_FinalProject/Classes/Drink.cs
+++ Group2_CS_FinalProject/Classes/Drink.cs
@@ -9,8 +9,8 @@ namespace Group2_CS_FinalProject.Classes
     public class Drink
 
     {
-    public static readonly string[] DrinkN = {"Coke", "Pepsi", "Dr.Pepper", "Sprite", "Water"};
-    public static readonly double[] DrinkP = {2.99, 2.99, 2.99, 2.99, 1.99};
+    public static readonly string[] DrinkN = {"Coke", "Pepsi", "Dr.Pepper", "Sprite"};
+    public static readonly double[] DrinkP = {2.99, 2.99, 2.99, 2.99};
     
     }
 }
